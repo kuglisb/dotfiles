c31d9db5b455774cebd20d64e67e417238beffa4
Balazs Szucs <balazs.szucs@me.com>
2024-04-20 20:46:09 +0200

    Update brew script

---
+++ brew.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+##################
+# Install Formulae
+##################
+# Java build tool
+brew install ant
+# Dependency manager for Cocoa projects
+brew install cocoapods
+# Distributed revision control system
+brew install git
+# Open-source build automation tool based on the Groovy and Kotlin DSL
+brew install gradle
+# Java-based project management
+brew install maven
+# Terminal-based visual file manager
+brew install midnight-commander
+# Ambitious Vim-fork focused on extensibility and agility
+brew install neovim
+# Platform built on V8 to build network applications
+brew install node
+# Code-search similar to ack
+brew install the_silver_searcher
+
+
+##################
+# Install Casks
+##################
+# (1Password) Password manager that keeps all passwords secure behind one password
+brew install --cask 1password
+# (Android SDK Command-line Tools) Command-line tools for building and debugging Android apps
+brew install --cask android-commandlinetools
+# (Android SDK Platform-Tools) Android SDK component
+brew install --cask android-platform-tools
+# (.NET SDK) Developer platform
+brew install --cask dotnet-sdk
+# (Mozilla Firefox) Web browser
+brew install --cask firefox
+# (Flutter SDK) UI toolkit for building applications for mobile, web and desktop
+brew install --cask flutter
+# (Google Chrome) Web browser
+brew install --cask google-chrome
+# (Home Assistant) Companion app for Home Assistant home automation software
+brew install --cask home-assistant
+# (iTerm2) Terminal emulator as alternative to Apple's Terminal app
+brew install --cask iterm2
+# (Notion) App to write, plan, collaborate, and get organised
+brew install --cask notion
+# (Slack) Team communication and collaboration software
+brew install --cask slack
+# (Transmission Remote GUI) [no description]
+brew install --cask transmission-remote-gui
+# (Microsoft Visual Studio Code, VS Code) Open-source code editor
+brew install --cask visual-studio-code
+# (VLC media player) Multimedia player
+brew install --cask vlc
