ee47f0e1a6a442c01aff0560fb49f2c4590ff362
Balazs Szucs <balazs.szucs@me.com>
2024-04-29 15:22:48 +0200

    Executable bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,6 +5,7 @@ cd "$(dirname "${BASH_SOURCE}")";
 rsync --exclude ".git/" \
     --exclude ".DS_Store" \
     --exclude "bootstrap.sh" \
+    --exclude "bootstrap-win.sh" \
     --exclude "brew.sh" \
     --exclude "README.md" \
     --exclude "LICENSE.md" \
