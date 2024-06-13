f91c86a4e7efd89b24854521c6db93b42cec8101
Balazs Szucs <balazs.szucs@tresorit.com>
2024-06-13 11:23:48 +0200

    Add brew-work.sh

---
+++ brew-work.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+##################
+# Install Formulae
+##################
+# Automatically formats Python code to conform to the PEP 8 style guide
+brew install autopep8
+# Terminal-based visual file manager
+brew install midnight-commander
+# Ambitious Vim-fork focused on extensibility and agility
+brew install neovim
+# Code-search similar to ack
+brew install the_silver_searcher
+
+##################
+# Install Casks
+##################
+# (1Password) Password manager that keeps all passwords secure behind one password
+brew install --cask 1password
+# (.NET SDK) Developer platform
+brew install --cask dotnet-sdk
+# (Google Chrome) Web browser
+brew install --cask google-chrome
+# (iTerm2) Terminal emulator as alternative to Apple's Terminal app
+brew install --cask iterm2
+brew install --cask microsoft-remote-desktop
+brew install --cask parallels
+brew install --cask spotify
+# (Microsoft Visual Studio Code, VS Code) Open-source code editor
+brew install --cask visual-studio-code
+# (VLC media player) Multimedia player
+brew install --cask vlc
